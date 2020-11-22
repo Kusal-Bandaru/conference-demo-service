ec2a3c3a3dbed1002725125ce57c25f174307c41
Kusal-Bandaru <kusal.bandaru6@gmail.com>
2020-11-22 22:04:03 +0530

    Adding @CrossOrigin to resolve CORS

--- src/main/java/com/kusal/conferencedemo/controller/HomeController.java
+++ src/main/java/com/kusal/conferencedemo/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.kusal.conferencedemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 public class HomeController {
 	
--- src/main/java/com/kusal/conferencedemo/controller/SessionController.java
+++ src/main/java/com/kusal/conferencedemo/controller/SessionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kusal.conferencedemo.model.Session;
 import com.kusal.conferencedemo.repository.SessionRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionController {
